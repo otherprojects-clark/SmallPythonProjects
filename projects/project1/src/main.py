90ab5f9b942afc95c3c825d62e4bff60a1c9d9c7
Clark <daprogrammer1@gmail.com>
2022-04-10 16:20:54 +0800

    update font tag to new

--- projects/project1/src/main.py
+++ projects/project1/src/main.py
@@ -1,12 +1,24 @@
 from colorama import init, Fore
-from platform import system
+from sys import platform
 
-if system() == 'Windows':
+if platform == 'win32':
   init()
 else:
   pass
 
+with open('users.txt', 'r') as a:
+  users = a.readlines()
+with open('passwords.txt', 'r') as b:
+  pws = b.readlines()
+
+
 print('Welcome to this Password Manager!')
+if users == [] and pws == []:
+  print('Let\'s create your account')
+
+
 password = input()
+
+
 
 # Startup
